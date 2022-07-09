33af2acebcdb9e1dcbab207ec6cc8c79f23f1b40
sivepanda <sivenpanda@icloud.com>
2022-07-09 00:05:27 -0500

    initial

---
+++ with-tailwindcss-app/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+// import Navbar from 'next/navbar'
+// import Footer from 'next/footer'
+
+const Home: NextPage = () => {
+  return (
+    <html className='bg-slate-900'>
+
+    
+      <div className='p-6 max-w-sm mx-auto bg-slate-700 rounded-xl shadow-lg items-center'>
+            <div className="text-xl font-medium text-white">ChitChat</div>
+      </div>
+    </html>
+  )
+}
+
+export default Home
